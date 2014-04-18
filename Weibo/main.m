56ff931fcc66d9f7f3bed9bb20e126ed3276ce38
Intern <intern@MaxMaxdeiMac.local>
2014-04-18 16:15:17 +0800

    Initial Commit

---
+++ Weibo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Weibo
+//
+//  Created by Intern on 14-4-18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
